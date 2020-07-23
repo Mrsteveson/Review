0dc401e9ba45efcc4cccfddfd425f72ced96e562
mrsteveson <patrick.steveson727@gmail.com>
2020-07-23 17:06:19 -0500

    improved solution included

--- coaching_sessions/bigger_is_greater/bigger_is_greater.py
+++ coaching_sessions/bigger_is_greater/bigger_is_greater.py
@@ -37,3 +37,41 @@ def biggerIsGreater(w):
     # character in the string is "h"
     # so we need to replace "d" with "h"
     # then put the other characters in lexicographically sorted order after "h"
+
+def bigger_is_greater(w):
+	# turn the input string into a list of chars
+	chars = list(w)
+	# index for when we traverse the list from right to left
+	# in order to find the char that needs to be swapped
+	i = len(chars) - 1
+	# loop through the list until we find the char to swap
+	while i > 0 and chars[i-1] >= chars[i]:
+		i -= 1
+	# if i gets all the way to the left of the array, then we 
+	# didn't find a char that needs to be swapped out
+	if i <= 0:
+		return "no answer"
+	
+	# once we've found the char to swap, perform another traversal
+	# starting from the right of the list to the left in order to 
+	# find the smallest char that is greater than the char that 
+	# needs to be swapped
+	while chars[j] <= chars[i-1]:
+		j -= 1
+	# at this point, j is pointing at the smallest char that is 
+	# greater than the char that needs to be swapped
+	# swap them 
+	chars[i-1], chars[j] = chars[j], chars[i-1]
+	
+	# now we need to change the string so that every char to the 
+	# right of the swapped char is sorted in lexicographical order
+	# it turns out we don't need to perform a full-on sort here
+	# because it turns out that any chars we didn't swap are 
+	# already in descending lexicographical order 
+	# so, instead of performing a full-on sort, all we need to
+	# do is reverse the sub-list starting at the char directly
+	# to the right of the swapped char
+	chars[i:] = chars[len(chars)-1 : i-1 : -1]
+	
+	# finally, join the list of chars back into a string
+	return "".join(chars)
